508a195e1015b7a837024fabee32cc5a57164ed1
Henny Feliz <66764084+hennyfeliz@users.noreply.github.com>
2023-08-01 22:08:23 -0400

    Capa de application para users

---
+++ back/src/main/java/com/security/application/service/UserService.java
@@ -0,0 +1,46 @@
+package com.security.application.service;
+
+import com.security.domain.model.User;
+import com.security.domain.ports.in.UserUseCase;
+
+import java.util.List;
+import java.util.Optional;
+
+public class UserService implements UserUseCase {
+    public final UserUseCase userUseCase;
+
+    public UserService(UserUseCase userUseCase) {
+        this.userUseCase = userUseCase;
+    }
+
+    @Override
+    public User createUser(User user) {
+        return userUseCase.createUser(user);
+    }
+
+    @Override
+    public Optional<User> getUserById(Long id) {
+        return userUseCase.getUserById(id);
+    }
+
+    @Override
+    public Optional<User> getUserByEmail(String email) {
+        return Optional.empty();
+    }
+
+
+    @Override
+    public List<User> getAllUsers() {
+        return userUseCase.getAllUsers();
+    }
+
+    @Override
+    public Optional<User> updateUser(Long id, User user) {
+        return userUseCase.updateUser(id, user);
+    }
+
+    @Override
+    public boolean deleteUser(Long id) {
+        return userUseCase.deleteUser(id);
+    }
+}
---
+++ back/src/main/java/com/security/application/usecase/UserUseCaseImpl.java
@@ -0,0 +1,46 @@
+package com.security.application.usecase;
+
+import com.security.domain.model.User;
+import com.security.domain.ports.in.UserUseCase;
+import com.security.domain.ports.out.UserRepositoryPort;
+
+import java.util.List;
+import java.util.Optional;
+
+public class UserUseCaseImpl implements UserUseCase {
+    public final UserRepositoryPort userRepositoryPort;
+
+    public UserUseCaseImpl(UserRepositoryPort userRepositoryPort) {
+        this.userRepositoryPort = userRepositoryPort;
+    }
+
+    @Override
+    public User createUser(User user) {
+        return userRepositoryPort.save(user);
+    }
+
+    @Override
+    public Optional<User> getUserById(Long id) {
+        return userRepositoryPort.findById(id);
+    }
+
+    @Override
+    public Optional<User> getUserByEmail(String email) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userRepositoryPort.findAll();
+    }
+
+    @Override
+    public Optional<User> updateUser(Long id, User user) {
+        return userRepositoryPort.update(user);
+    }
+
+    @Override
+    public boolean deleteUser(Long id) {
+        return userRepositoryPort.deleteById(id);
+    }
+}
