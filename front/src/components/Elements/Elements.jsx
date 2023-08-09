85f708d91e610698d64dc1c47183fbaecf9d73a8
Henny Feliz <66764084+hennyfeliz@users.noreply.github.com>
2023-08-09 03:44:15 -0400

    Cambios componente y estilos elements

--- front/src/components/Elements/Elements.jsx
+++ front/src/components/Elements/Elements.jsx
@@ -1,26 +1,27 @@
 import '../../Styles/Elements.css'
 import productIcon from './assets/error.svg'
+import Modal from '../Modal/Modal'
+import { useState } from 'react'
 
 const Elements = () => {
 
-  const elements = [
-    {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedModalData, setSelectedModalData] = useState({}); // Datos para el modal
 
-    }, {
-    }, {
-    }, {
-    }, {
-    }, {
-    }, {
-    }, {
-    }, {
-    }, {
-    }, {
-    }, {
-    }, {
-    }, {
-    }
-  ]
+  const elements = [
+    { id: 1, name: 'zapatos', description: 'zapatos baratos' },
+    { id: 2, name: 'corbata', description: 'corbata barata' },
+    // ... otros elementos
+  ];
+
+  const openModal = (elements) => {
+    setSelectedModalData(elements);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
 
   return (
@@ -29,17 +30,24 @@ const Elements = () => {
         {
           elements.map((item, index) => {
             return (
-              <div className="element" key={index}>
-                <img
-                  alt="product-icon"
-                  src={productIcon}
-                  style={{ width: "200px", height: "200px", margin: "10px auto" }}
-                />
-                <div>
-                  <span>$200</span>
-                  <h3>zapatos</h3>
-                  <span>zapatos baratos</span>
+              <div key={index}>
+                <div className="element" onClick={openModal}>
+                  <img
+                    alt="product-icon"
+                    src={productIcon}
+                    style={{ width: "200px", height: "200px", margin: "10px auto" }}
+                  />
+                  <div>
+                    <span>$200</span>
+                    <h3>{item.name} </h3>
+                    <span>{item.description} </span>
+                  </div>
                 </div>
+                <Modal
+                  isOpen={isModalOpen}
+                  closeModal={closeModal}
+                  modalData={selectedModalData}
+                />
               </div>
             )
           })
