06d2f54728c353980b2ce67b206dfc4d4d9c1c52
Henny Feliz <66764084+hennyfeliz@users.noreply.github.com>
2023-08-10 00:12:26 -0400

    Cambios componente Modal y estilos del mismo

--- front/src/components/Modal/Modal.jsx
+++ front/src/components/Modal/Modal.jsx
@@ -1,37 +1,40 @@
-import { useSpring, animated } from 'react-spring'; // Importa la librería de animaciones
 import './Modal.css'
 
-const Modal = ({ isOpen, closeModal, modalData }) => {
-  const animation = useSpring({
-    from: { transform: 'translateY(100%)', opacity: 0 },
-    to: { transform: 'translateY(0)', opacity: 1 },
-    config: { tension: 200, friction: 20 },
-    // Cambia los valores de 'tension' y 'friction' para ajustar la animación
-  });
-
-  const agregarAlCarrito = () => {
-    alert("producto agregado al carrito con éxito!")
-  }
+const Modal = ({ isOpen, closeModal, data }) => {
 
   return (
-    <animated.div
+    <div
       className={`modal ${isOpen ? 'open' : ''}`}
-      style={animation}
     >
       <div className="modal-content">
         <h1>Información producto</h1>
-        <h2>{modalData.name ?? "nombre producto" }</h2>
-        <p>{modalData?.description ?? "descripción producto"}</p>
-        {/* Otros elementos y datos relacionados */}
+        <h2>{data.name}</h2>
+        <>
+          <h3>Descripción:</h3>
+          <span> {data.cartDesc}</span>
+        </>
+        <>
+          <h3>Precio:</h3>
+          <span> {data.price}</span>
+        </>
+        <>
+          <h3>Stock:</h3>
+          <span> {data.stock}</span>
+        </>
+        <>
+          <h3>Localización:</h3>
+          <span> {data.location}</span>
+        </>
+        <>
+          <h3>Descripción completa:</h3>
+          <span className="descripcion"> {data.longDesc}</span>
+        </>
         <div className="buttons">
-          <button className="modal-button green" onClick={() => {
-            agregarAlCarrito()
-            closeModal()
-          }}>Agregar al carrito</button>
+          <button className="modal-button gray" onClick={closeModal}>Agregar al carrito</button>
           <button className="modal-button gray" onClick={closeModal}>Cerrar</button>
         </div>
       </div>
-    </animated.div>
+    </div>
   );
 };
 
