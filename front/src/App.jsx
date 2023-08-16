bfb6d95d2e007f0471a83e1fad576e631d034e4d
Henny Feliz <66764084+hennyfeliz@users.noreply.github.com>
2023-08-16 05:14:54 -0400

    Cambios archivos base front

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,15 +1,26 @@
-import { useState } from 'react'
 import './App.css'
 import Header from './components/Header/Header'
 import Elements from './components/Elements/Elements'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Login from './components/Login/Login'
+import UserDashboard from './components/UserDashBoard/UserDashboard'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Header />
-      <Elements />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Header />
+              <Elements />
+            </>
+          } />
+          <Route path="/login" element={<Login />} />
+          <Route path="/user" element={<UserDashboard />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
