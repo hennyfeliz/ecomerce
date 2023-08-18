0af83617abcd80ac1f0daa3890cabbce87e021f1
Henny Feliz <66764084+hennyfeliz@users.noreply.github.com>
2023-08-17 22:56:23 -0400

    Cambios componente modal y estilos

--- front/src/components/Modal/Modal.jsx
+++ front/src/components/Modal/Modal.jsx
@@ -1,4 +1,6 @@
 import './Modal.css'
+import icon from "../Elements/assets/error.svg"
+
 
 const Modal = ({ isOpen, closeModal, data }) => {
 
@@ -6,32 +8,40 @@ const Modal = ({ isOpen, closeModal, data }) => {
     <div
       className={`modal ${isOpen ? 'open' : ''}`}
     >
-      <div className="modal-content">
-        <h1>Información producto</h1>
-        <h2>{data.name}</h2>
-        <>
-          <h3>Descripción:</h3>
-          <span> {data.cartDesc}</span>
-        </>
-        <>
-          <h3>Precio:</h3>
-          <span> {data.price}</span>
-        </>
-        <>
-          <h3>Stock:</h3>
-          <span> {data.stock}</span>
-        </>
-        <>
-          <h3>Localización:</h3>
-          <span> {data.location}</span>
-        </>
-        <>
-          <h3>Descripción completa:</h3>
-          <span className="descripcion"> {data.longDesc}</span>
-        </>
-        <div className="buttons">
-          <button className="modal-button gray" onClick={closeModal}>Agregar al carrito</button>
-          <button className="modal-button gray" onClick={closeModal}>Cerrar</button>
+      <div className="product-content-information">
+        <div>
+          <img 
+          style={{width: "300px", height: "300px"}}
+            src={icon}
+          />
+        </div>
+        <div className="modal-content">
+          <h1>Información producto</h1>
+          <h2>{data.name}</h2>
+          <>
+            <h3>Descripción:</h3>
+            <span> {data.cartDesc}</span>
+          </>
+          <>
+            <h3>Precio:</h3>
+            <span> {data.price}</span>
+          </>
+          <>
+            <h3>Stock:</h3>
+            <span> {data.stock}</span>
+          </>
+          <>
+            <h3>Localización:</h3>
+            <span> {data.location}</span>
+          </>
+          <>
+            <h3>Descripción completa:</h3>
+            <span className="descripcion"> {data.longDesc}</span>
+          </>
+          <div className="buttons">
+            <button className="modal-button gray" onClick={closeModal}>Agregar al carrito</button>
+            <button className="modal-button gray" onClick={closeModal}>Cerrar</button>
+          </div>
         </div>
       </div>
     </div>
